abe12aae6ade77fe4d863852b15521aa833fb653
myholt <myles@slcket.com>
2025-03-06 14:49:22 -0500

    setup supabase db and test fetch

--- src/app/drizzle.tsx
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase client
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
-
-// Drizzle ORM initialization
-export const db = drizzle(supabase);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,41 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import supabase from "@/app/config/supabaseClient";
+
 export default function Home() {
+
+  interface User {
+    id: number,
+    email: string,
+    name: string,
+  }
+
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {error, data} = await supabase.from("User").select()
+      if (error) {
+        console.error("Error fetching users:", error.message)
+      }
+      if (data) {
+        setUsers(data)
+      }
+    }
+    fetchUsers()
+  }, []);
+
   return (
     <div>
       <main>
-        <div>Home</div>
+        <ul>
+          {users.map(user => {
+            return (
+              <li>{user.name} {user.email}</li>
+            )
+          })}
+        </ul>
       </main>
       <footer></footer>
     </div>
