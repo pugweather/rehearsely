0c6cd2fb9ebdf233ad12cc2fdc0c0d73dc9ea7a9
myholt <myles@slcket.com>
2025-03-11 20:41:00 -0400

    replace scene wrapper w/ editorWrapper component

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Navbar from "../components/layout/Navbar";
+import EditorWrapper from "../components/editor/EditorWrapper";
 
 const Editor = () => {
     return (
         <div className="min-h-screen bg-gray-200 flex flex-col">
             <Navbar />
-            <div className="flex-grow max-w-[50rem] w-full bg-white mx-auto">
-
-            </div>
+            <EditorWrapper></EditorWrapper>
         </div>
     )
 }
