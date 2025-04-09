cae3a1867284cd118c3ca9895a51ef6637397fe4
myholt <myles@slcket.com>
2025-04-09 00:31:40 -0400

    set link to home in navbar, pass scene into editor + add scene name to editor

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Navbar() {
+
+    const handleHomeClick = () => {
+        
+    }
+
     return (
         <nav className="w-full h-20 px-5 flex justify-between items-center bg-white border-b border-gray-300">
-            <div className="font-bold text-xl">Rehearsely</div>
+            <Link href={"/"} className="font-bold text-xl cursor-pointer">Rehearsely</Link>
             <div className="w-10 h-10 rounded-full flex justify-center items-center bg-blue-500 text-white text-lg">M</div>
         </nav>
     )
--- src/app/editor/[sceneId]/page.tsx
+++ src/app/editor/[sceneId]/page.tsx
@@ -1,18 +1,35 @@
-import React from "react";
 import Link from "next/link";
 import Navbar from "../../components/layout/Navbar";
 import EditorWrapper from "../../components/editor/EditorWrapper";
 import SceneSettings from "../../components/editor/SceneSettings";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { scenes } from "@/database/drizzle/schema";
+import db from "@/app/database";
+import { eq } from "drizzle-orm";
 
-const Editor = () => {
+type Props = {
+    params: {
+        sceneId: string;
+    }
+}
+
+const Editor = async ({params}: Props) => {
+
+    const {sceneId} = params
+    const sceneRes = await db
+        .select()
+        .from(scenes)
+        .where(eq(scenes.id, Number(sceneId)))
+    const scene = sceneRes[0] || null
+
     return (
         <div className="min-h-screen bg-gray-200 flex flex-col">
             <Navbar />
             <EditorWrapper>
-                <div className="text-gray-500 py-6 border-b border-b-gray-300">
+                <div className="relative text-gray-500 py-6 border-b border-b-gray-300">
                     <Link href="/scenes-dashboard"><FontAwesomeIcon icon={faArrowLeftLong} /> <span className="ml-1">Back to Scenes</span></Link>
+                    <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-xl font-medium text-gray-900">{scene.name}</div>
                 </div>
                 <SceneSettings />
                 <div className="flex flex-col items-center py-8">
