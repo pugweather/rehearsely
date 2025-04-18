83c5ac641109c488d61e190c52edab6be501d557
myholt <myles@slcket.com>
2025-04-18 01:31:27 -0400

    new logo for navbar

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -6,12 +6,12 @@ export default function Navbar() {
 
     return (
         <nav className="w-full h-20 px-5 flex justify-between items-center bg-white border-b border-gray-300">
-            <Link href={"/"} className="font-bold text-xl cursor-pointer relative" style={{ width: "150px", height: "75px" }}>
+            <Link href={"/"} className="font-bold text-xl cursor-pointer relative" style={{ width: "75px", height: "75px" }}>
             <Image
-                src="/logo.png"
+                src="/rehearsely-jester.png"
                 alt="Rehearsely logo"
                 fill
-                style={{objectFit: "cover"}}
+                style={{objectFit: "contain"}}
             />
             </Link>
             <div className="w-10 h-10 rounded-full flex justify-center items-center bg-blue-500 text-white text-lg cursor-pointer">M</div>
